e7fa93cca014abb401bb4d2c090cf076a9f2587b
Renaldy <renaldy_riz@yahoo.com>
2021-05-17 16:36:57 +0700

    feat: Project Initialization

---
+++ DBQuery.sql
@@ -0,0 +1,62 @@
+﻿GO
+CREATE TABLE [dbo].[MsMember]
+(
+	[MemberID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[MemberName] VARCHAR(255) NOT NULL,
+	[MemberDOB] DATE NOT NULL,
+	[MemberGender] VARCHAR(255) NOT NULL,
+	[MemberAddress] VARCHAR(255) NOT NULL,
+	[MemberPhone] VARCHAR(255) NOT NULL,
+	[MemberEmail] VARCHAR(255) NOT NULL,
+	[MemberPassword] VARCHAR(255) NOT NULL
+)
+
+GO
+CREATE TABLE [dbo].[MsFlowerType]
+(
+	[FlowerTypeID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[FlowerTypeName] VARCHAR(255) NOT NULL
+)
+
+GO
+CREATE TABLE [dbo].[MsFlower]
+(
+	[FlowerID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[FlowerName] VARCHAR(255) NOT NULL,
+	[FlowerTypeID] INT NOT NULL FOREIGN KEY REFERENCES [MsFlowerType](FlowerTypeID),
+	[FlowerDescription] VARCHAR(255) NOT NULL,
+	[FlowerPrice] INT NOT NULL,
+	[FlowerImage] NVARCHAR(260) NOT NULL
+)
+
+GO
+CREATE TABLE [dbo].[MsEmployee]
+(
+	[EmployeeID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[EmployeeName] VARCHAR(255) NOT NULL,
+	[EmployeeDOB] DATE NOT NULL,
+	[EmployeeGender] VARCHAR(255) NOT NULL,
+	[EmployeeAddress] VARCHAR(255) NOT NULL,
+	[EmployeePhone] VARCHAR(255) NOT NULL,
+	[EmployeeEmail] VARCHAR(255) NOT NULL,
+	[EmployeeSalary] INT NOT NULL,
+	[EmployeePassword] VARCHAR(255) NOT NULL
+)
+
+GO
+CREATE TABLE [dbo].[TrHeader]
+(
+	[TransactionID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[MemberID] INT NOT NULL FOREIGN KEY REFERENCES [MsMember](MemberID),
+	[EmployeeID] INT NOT NULL FOREIGN KEY REFERENCES [MsEmployee](EmployeeID),
+	[TransactionDate] DATE NOT NULL,
+	[DiscountPercentage] FLOAT NOT NULL
+)
+
+GO
+CREATE TABLE [dbo].[TrDetail]
+(
+	[TransactionID] INT NOT NULL FOREIGN KEY REFERENCES [TrHeader](TransactionID),
+	[FlowerID] INT NOT NULL FOREIGN KEY REFERENCES [MsFlower](FlowerID),
+	[Quantity] INT NOT NULL
+)
---
+++ NeinteenFlower/View/Login.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="NeinteenFlower.View.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="txtTitle" runat="server" Text="Login" Font-Bold="true"></asp:Label>
+        </div>
+
+        <div>
+            <asp:Label ID="lblEmail" runat="server" Text="Email: "></asp:Label>
+            <input id="txtEmail" type="text" type="email"/>
+        </div>
+
+        <div>
+            <asp:Label ID="lblPassword" runat="server" Text="Password: "></asp:Label>
+            <input id="txtPassword" type="password" />
+        </div>
+
+        <div>
+            <asp:Label ID="lblRememberMe" runat="server" Text="Remember Me"></asp:Label>
+            <input id="cbRememberMe" type="checkbox" />
+        </div>
+
+        <div>
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+        </div>
+
+        <br /><br />
+
+        <div>
+            <asp:HyperLink ID="hlRegister" runat="server" NavigateUrl="~/View/Register.aspx" Font-Underline="false">Register</asp:HyperLink>
+        </div>
+
+        <div>
+            <asp:HyperLink ID="hlForgotPassword" runat="server" NavigateUrl="~/View/ForgotPassword.aspx" Font-Underline="false">Forgot Password</asp:HyperLink>
+        </div>
+    </form>
+</body>
+</html>
