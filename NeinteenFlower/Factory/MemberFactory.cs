ed414866ae4a90fdada3959fb5e89dc56d7aadc0
Renaldy <renaldy_riz@yahoo.com>
2021-05-19 00:17:46 +0700

    feat: (Bagas)create member factory

--- NeinteenFlower/Factory/MemberFactory.cs
+++ NeinteenFlower/Factory/MemberFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NeinteenFlower.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,19 @@ namespace NeinteenFlower.Factory
 {
     public class MemberFactory
     {
+        public static MsMember createMember(string name, string gender, string address, string phone, string email, string password)
+        {
+            MsMember data = new MsMember
+            {
+                MemberName = name,
+                MemberGender = gender,
+                MemberAddress = address,
+                MemberPhone = phone,
+                MemberEmail = email,
+                MemberPassword = password
+            };
+
+            return data;
+        }
     }
 }
