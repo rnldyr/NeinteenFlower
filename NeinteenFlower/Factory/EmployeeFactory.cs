9a0e387487ff6d61393825a3ffdf5fa1497bf7d8
Renaldy <renaldy_riz@yahoo.com>
2021-05-18 22:08:43 +0700

    feat: add factory folder and files

---
+++ NeinteenFlower/Factory/EmployeeFactory.cs
@@ -0,0 +1,27 @@
+﻿using NeinteenFlower.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeinteenFlower.Factory
+{
+    public class EmployeeFactory
+    {
+        public static MsEmployee CreateEmployee(string name, string gender, string address, string phone, string email, int salary, string password)
+        {
+            MsEmployee data = new MsEmployee
+            {
+                EmployeeName = name,
+                EmployeeGender = gender,
+                EmployeeAddress = address,
+                EmployeePhone = phone,
+                EmployeeEmail = email,
+                EmployeeSalary = salary,
+                EmployeePassword = password
+            };
+
+            return data;
+        }
+    }
+}
