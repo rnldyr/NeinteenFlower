9ef749f3246f6174ba5b7b9465b366eb2eaf004f
Renaldy <renaldy_riz@yahoo.com>
2021-05-20 18:01:42 +0700

    feat: add member and employee controller and handler

---
+++ NeinteenFlower/Controller/MemberController.cs
@@ -0,0 +1,28 @@
+﻿using NeinteenFlower.Handler;
+using NeinteenFlower.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeinteenFlower.Controller
+{
+    public class MemberController
+    {
+        public static List<MsMember> getListMembers()
+        {
+            return MemberHandler.getListMembers();
+        }
+
+        public static void DeleteMember(int id)
+        {
+            MemberHandler.DeleteMember(id);
+            return;
+        }
+
+        public static int getMemberId(string name)
+        {
+            return MemberHandler.getMemberId(name);
+        }
+    }
+}
---
+++ NeinteenFlower/Handler/MemberHandler.cs
@@ -0,0 +1,28 @@
+﻿using NeinteenFlower.Model;
+using NeinteenFlower.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeinteenFlower.Handler
+{
+    public class MemberHandler
+    {
+        public static List<MsMember> getListMembers()
+        {
+            return MemberRepository.getListMembers();
+        }
+
+        public static void DeleteMember(int id)
+        {
+            MemberRepository.DeleteMember(id);
+            return;
+        }
+
+        public static int getMemberId(string name)
+        {
+            return MemberRepository.getMemberId(name);
+        }
+    }
+}
