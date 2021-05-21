33db47535c42ef9f4fc5607d51c38434dd673c52
Renaldy <renaldy_riz@yahoo.com>
2021-05-21 18:13:31 +0700

    fix: insert a new pk for trdetail table

--- NeinteenFlower/Model/TrDetail.cs
+++ NeinteenFlower/Model/TrDetail.cs
@@ -14,6 +14,7 @@ namespace NeinteenFlower.Model
     
     public partial class TrDetail
     {
+        public int TransactionDetailID { get; set; }
         public int TransactionID { get; set; }
         public int FlowerID { get; set; }
         public int Quantity { get; set; }
